b259997aa49393a0d6b8349a8788389069adb884
Andrea Chavarria <andrea.chavarria@unah.hn>
2021-02-28 12:00:09 -0600

    agregando modificaciones

--- Entregas/BL.Entregas/ClienteBL.cs
+++ Entregas/BL.Entregas/ClienteBL.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,38 +10,19 @@ namespace BL.Entregas
 {
     public class ClienteBL
     {
+        Contexto _contexto;
         public BindingList<Clientes> ListadeClientes { get; set; }
 
         public ClienteBL() // Constructor
         {
+            _contexto = new Contexto();
             ListadeClientes = new BindingList<Clientes>();
-
-            var cliente1 = new Clientes();
-            cliente1.CodigoCliente = 001;
-            cliente1.RTN = "0908999636134";
-            cliente1.NombredeEmpresa = "Repuestos y Mas";
-            cliente1.Direccion = "Barrio Suyapa, 9 clle, 10 Ave";
-            cliente1.Telefono = "2554-0630";
-            cliente1.Contacto = "Javier Martinez";
-            cliente1.Activo = true;
-
-            ListadeClientes.Add(cliente1);
-
-            var cliente2 = new Clientes();
-            cliente2.CodigoCliente = 002;
-            cliente2.RTN = "08015269471313";
-            cliente2.NombredeEmpresa = "Comercializadora de Productos";
-            cliente2.Direccion = "Colonia Las Mercedes, casa 20";
-            cliente2.Telefono = "9865-2424";
-            cliente2.Contacto = "Laura Perez";
-            cliente2.Activo = true;
-
-            ListadeClientes.Add(cliente2);
-
         }
 
         public BindingList<Clientes> ObtenerClientes()
         {
+            _contexto.Clientes.Load();
+            ListadeClientes = _contexto.Clientes.Local.ToBindingList();
             return ListadeClientes;
         }
 
---
+++ Entregas/BL.Entregas/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Entregas
+{
+    public class Contexto: DbContext
+    {
+        public Contexto(): base()
+        {
+
+        }
+
+        public DbSet<Clientes> Clientes { get; set; }
+    }
+
+}
